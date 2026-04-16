abff59080f782902b9648bd81a37b565f8302a79
zack965 <elhossnizakaria@gmail.com>
2026-04-17 00:10:33 +0100

    start doubly linked list

---
+++ src/DataStructure/LinkedList/Doubly/DoublyLinkedList.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Zack\PhpDsAlgo\DataStructure\LinkedList\Doubly;
+
+use IteratorAggregate;
+
+class DoublyLinkedList implements IteratorAggregate
+{
+    private ?DoublyLinkedListNode $head = null;
+    private int $length = 0;
+    private function __construct(?DoublyLinkedListNode $head = null, ?int $length = 0)
+    {
+        $this->head = $head;
+        $this->length = $length;
+    }
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+    public function getHead(): ?DoublyLinkedListNode
+    {
+        return $this->head;
+    }
+    public function getIterator(): \Traversable
+    {
+        $current = $this->head;
+
+        while ($current !== null) {
+            yield $current; // or yield $current->getValue();
+            $current = $current->getNext();
+        }
+    }
+}
