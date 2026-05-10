70f5e3883b2e12dcbd05218d4726ee7842a7a018
zack965 <elhossnizakaria@gmail.com>
2026-05-10 12:05:19 +0100

    add selection sort

--- src/Algorithmes/ArraySortAlgorythmes.php
+++ src/Algorithmes/ArraySortAlgorythmes.php
@@ -3,23 +3,37 @@
 
 namespace Zack\PhpDsAlgo\Algorithmes;
 
+use Zack\PhpDsAlgo\Helpers\Algorythmes\AlgorythmesGlobalHelpers;
+
 class ArraySortAlgorythmes
 {
-    public static function bubbleSort(array $nums):array
+    public static function bubbleSort(array $nums): array
     {
         $length_nums = count($nums);
-        for ($i = 1; $i < $length_nums ; $i++) {
-            for ($j = 0; $j < $length_nums -1 ; $j++) {
-                if($nums[$j] > $nums[$j+1]){
-                    self::swapValuesOfArray($nums,$j,$j+1);
+        for ($i = 1; $i < $length_nums; $i++) {
+            for ($j = 0; $j < $length_nums - 1; $j++) {
+                if ($nums[$j] > $nums[$j + 1]) {
+                    AlgorythmesGlobalHelpers::swapValuesOfArray($nums, $j, $j + 1);
                 }
             }
         }
         return $nums;
     }
-    public static function swapValuesOfArray(array &$nums , int $startIndex,int $endIndex):void {
-        $temp = $nums[$startIndex];
-        $nums[$startIndex] = $nums[$endIndex];
-        $nums[$endIndex] = $temp;
+
+    public static function selectionSort(array $nums): array
+    {
+        $length_nums = count($nums);
+        for ($i = 0; $i <= $length_nums - 1; $i++) {
+            $minimumIndex = $i;
+            for ($j = $i + 1; $j <= $length_nums - 1; $j++) {
+                if ($nums[$j] < $nums[$minimumIndex]) {
+                    $minimumIndex = $j;
+                }
+            }
+            if ($minimumIndex !== $i) {
+                AlgorythmesGlobalHelpers::swapValuesOfArray($nums, $i, $minimumIndex);
+            }
+        }
+        return $nums;
     }
 }
--- src/Helpers/Algorythmes/AlgorythmesGlobalHelpers.php
+++ src/Helpers/Algorythmes/AlgorythmesGlobalHelpers.php
@@ -2,10 +2,27 @@
 
 
 namespace Zack\PhpDsAlgo\Helpers\Algorythmes;
+
+use InvalidArgumentException;
+
 class AlgorythmesGlobalHelpers
 {
     public static function isBetween(int $number, int $lower, int $upper): bool
     {
         return $number >= $lower && $number <= $upper;
+    }
+    public static function swapValuesOfArray(array &$nums, int $startIndex, int $endIndex): void
+    {
+
+        if (!array_key_exists($startIndex, $nums)) {
+            throw new InvalidArgumentException("Start index {$startIndex} does not exist in the array.");
+        }
+
+        if (!array_key_exists($endIndex, $nums)) {
+            throw new InvalidArgumentException("End index {$endIndex} does not exist in the array.");
+        }
+        $temp = $nums[$startIndex];
+        $nums[$startIndex] = $nums[$endIndex];
+        $nums[$endIndex] = $temp;
     }
 }
